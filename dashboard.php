1e74747a2e61c4043667155f107432a59bf5a04d
Seane630 <seane63000@gmail.com>
2018-12-24 15:16:58 -0600

    created sidebar for admin dashboard and started main dashboard area.. added css styling

--- dashboard.php
+++ dashboard.php
@@ -9,15 +9,34 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.0/css/all.css" integrity="sha384-aOkxzJ5uQz7WBObEZcHvV5JvRW3TUc2rNPA7pe3AwnsUohiw1Vj2Rgx2KSOkF5+h"
         crossorigin="anonymous">
         <!-- CSS --> 
-    <link rel="stylesheet" href="/css/adminStyles.css ">
+    <link rel="stylesheet" href="css/adminStyles.css ">
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <!-- Latest compiled JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <title>Document</title>
+    <title>Admin Dashboard</title>
 </head>
 <body>
-    
+    <div class='container-fluid'>
+        <div class="row">
+            <div class="col-sm-2">
+                <h2>Hello Sean!</h2>
+                <ul id="side_menu" class="nav nav-pills nav-stacked">
+                    <li class="active"><a href="Dashboard.php"><span class="fas fa-bars"></span>&nbsp;Dashboard</a></li>
+                    <li><a href="#"><span class="fas fa-plus"></span>&nbsp;Add New Post</a></li>
+                    <li><a href="#"><span class="fas fa-comments"></span>&nbsp;Comments</a></li>
+                    <li><a href="#"><span class="fas fa-tags"></span>&nbsp;Categories</a></li>
+                    <li><a href="#"><span class="fas fa-user"></span>&nbsp;Manage Admins</a></li>
+                    <li><a href="#"><span class="fas fa-satellite-dish"></span>&nbsp;Live Blog</a></li>
+                    <li><a href="#"><span class="fas fa-sign-out-alt"></span>&nbsp;Logout</a></li>
+                </ul>
+             </div> <!--End of Sidebar -->
+            <div class="col-sm-10">
+                <h1>Admin Dashboard</h1>
+                <p>ard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing L</p>
+            </div> <!--End of Main Area -->
+        </div> <!--End Row -->
+    </div> <!--End Container Fluid -->
 </body>
 </html>
